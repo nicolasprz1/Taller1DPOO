c79be91b178f95ee99aea793ba0c874000008d90
nicolasprz1 <n.perezr@uniandes.edu.co>
2023-08-22 19:47:41 -0500

    Primer commit

---
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,23 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc = null;
+		try {
+			calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(calc.paisConMasMedallistas());
+	}
+
+}
